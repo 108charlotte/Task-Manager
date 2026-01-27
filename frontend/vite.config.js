b5ac8d44367c58b8a9022dcabedf2a97fc91d501
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-01-27 14:20:51 -0500

    updated vite config to not handle api routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,28 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, 
-    allowedHosts: ['taskmanager.is-a.software']
+    allowedHosts: ['taskmanager.is-a.software'], 
+    proxy: {
+      '/authentication': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/tasks': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/addtask': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/deletetask': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/admin': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
   }
 })
